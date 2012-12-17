a1bad5b198e10d53d68a5550199f05627bbdbe64
Jakob Sievers <canned.primat.es@gmail.com>
2012-12-17 17:05:57 +0100

    import some stuff

--- src/clj_libcrawl/core.clj
+++ src/clj_libcrawl/core.clj
@@ -3,15 +3,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;_* Declarations =====================================================
-(ns clj-libcrawl.core)
+(ns clj-libcrawl.core
+  (:require [clj-libcrawl.search  :as search])
+  (:require [clj-libcrawl.twitter :as twitter])
+  (:require [clj-libcrawl.http    :as http])
+  )
 
 ;;;_* Code =============================================================
 (defn -main
   "Suggest friends for SCREEN-NAME."
   [screen-name]
   (try
-    (clj-libcrawl.search/suggest
-     (clj-libcrawl.twitter/screen-name->node screen-name))
+    (search/suggest (twitter/screen-name->node screen-name))
     (future-cancel http/bgtask)
     (System/exit 0)
     (catch Exception e (clojure.stacktrace/print-stack-trace e))))
